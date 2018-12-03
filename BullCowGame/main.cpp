b269438a034f0ad78b1600478435a99da0a67c44
shawn <shawnantonucci2018@gmail.com>
2018-12-02 20:17:52 -0800

    get a guess and return it

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	constexpr int WORLD_LENGTH = 5;
-	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
+	// introduce the game
+	constexpr int WORLD_LENGTH = 9;
+
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORLD_LENGTH;
 	cout << " letter isogram I'm thinking of?\n";
+	cout << endl;
+
+	// get a guess from the player
+	cout << "Enter your guess: ";
+	string Guess = "";
+	cin >> Guess;
+
+	// repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+
+	// get a guess from the player
+	cout << "Enter your guess: ";
+	cin >> Guess;
+
+	// repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+
+	cout << endl;
 	return 0;
 }
