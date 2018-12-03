130f0c33dffd375da9efbfe5ed24f9d989c68bd8
shawn <shawnantonucci2018@gmail.com>
2018-12-02 21:06:52 -0800

    added a playGame and GetGuess function

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -4,22 +4,19 @@
 using namespace std;
 
 void PrintIntro();
-string GetGuessAndPrintBack();
+void PlayGame();
+string GetGuess();
+
+
 
 // the entry point for our application
 int main()
 {
 	PrintIntro();
-
-	// loop for the number of turns asking for guesses
-	constexpr int NUMBER_OF_TURNS = 5;
-	for (int count = 1; count <= NUMBER_OF_TURNS; count++) {
-		GetGuessAndPrintBack();
-		cout << endl;
-	}
-
-	return 0;
+	PlayGame();
+	return 0; // exit the application
 }
+
 
 	// introduce the game
 void PrintIntro() 
@@ -32,14 +29,22 @@ void PrintIntro()
 	return;
 }
 
-string GetGuessAndPrintBack() 
+void PlayGame()
+{
+	// loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++) {
+		string Guess = GetGuess();
+		cout << "Your guess was: " << Guess << endl;
+		cout << endl;
+	}
+}
+
+string GetGuess() 
 {
 	// get a guess from the player
 	cout << "Enter your guess: ";
 	string Guess = "";
 	getline(cin, Guess);
-
-	// print the guess back to them
-	cout << "Your guess was: " << Guess << endl;
 	return Guess;
 }
