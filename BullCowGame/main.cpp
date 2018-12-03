bb4b17e40c96d5aa31e2e9abfc9fd2e652f298c6
shawn <shawnantonucci2018@gmail.com>
2018-12-02 19:16:56 -0800

    added a word length and print out that number

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
-	cout << "Welcome to Bulls and Cows" << endl;
+	constexpr int WORLD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
+	cout << "Can you guess the " << WORLD_LENGTH;
+	cout << " letter isogram I'm thinking of?\n";
 	return 0;
 }
