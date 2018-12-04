5f4e3ae2beef3e45e860a5b0dbafe0a5c0d28128
shawn <shawnantonucci2018@gmail.com>
2018-12-03 22:32:51 -0800

    created game functions on cpp file

--- BullCowGame/FBullCowGame.h
+++ BullCowGame/FBullCowGame.h
@@ -1,12 +1,13 @@
 #pragma once
+#include <string>
 
 class FBullCowGame {
 public:
 	void Reset(); // TODO make a more rich return value.
 	int GetMaxTries();
-	int GetCurrentTry;
+	int GetCurrentTry();
 	bool IsGameWon();
-	bool CheckGuessValidity(string); // TODO make a more rich return value.
+	bool CheckGuessValidity(std::string); // TODO make a more rich return value.
 
 // Pleasse try and ignore this and focus on the interface abot ^^
 private:
