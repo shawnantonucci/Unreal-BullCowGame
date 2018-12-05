7a4f5973d3db098d60134b73cce4cea3bb1eb8b3
shawn <shawnantonucci2018@gmail.com>
2018-12-04 22:31:50 -0800

    restructured submit guess

--- BullCowGame/FBullCowGame.cpp
+++ BullCowGame/FBullCowGame.cpp
@@ -15,7 +15,7 @@ void FBullCowGame::Reset()
 	constexpr int32 MAX_TRIES = 8;
 	MyMaxTries = MAX_TRIES;
 
-	const FString HIDDEN_WORD = "planet";
+	const FString HIDDEN_WORD = "ant";
 	MyHiddenWord = HIDDEN_WORD;
 
 	MyCurrentTry = 1;
@@ -43,17 +43,20 @@ FBullCowCount FBullCowGame::SubmitGuess(FString Guess)
 
 	// loop through all letters in the guess
 	int32 HiddenWordLength = MyHiddenWord.length();
-	for (int32 i = 0; i < HiddenWordLength; i++) 
+	for (int32 MHWChar = 0; MHWChar < HiddenWordLength; MHWChar++) 
 	{
 		// compare letters against the hidden word
-		for (int32 j = 0; j < HiddenWordLength; j++)
+		for (int32 GChar = 0; GChar < HiddenWordLength; GChar++)
 		{
 			// if they match then
-			if (Guess[i] == MyHiddenWord[i]) {
-				if (i == j) { // if they're in the same place
+			if (Guess[GChar] == MyHiddenWord[MHWChar]) 
+			{
+				if (MHWChar == GChar) 
+				{ // if they're in the same place
 					BullCowCount.Bulls++; // incriments bulls
 				}
-				else {
+				else 
+				{
 					BullCowCount.Cows++; // increment cowss
 
 				}
