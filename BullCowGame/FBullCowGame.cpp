e77c840e2ade61b0b60849c5e671cf2519d5417c
shawn <shawnantonucci2018@gmail.com>
2018-12-04 23:10:40 -0800

    added enum and restructered some code

--- BullCowGame/FBullCowGame.cpp
+++ BullCowGame/FBullCowGame.cpp
@@ -2,22 +2,20 @@
 
 using int32 = int;
 
-FBullCowGame::FBullCowGame()
-{
-	Reset();
-}
-
+FBullCowGame::FBullCowGame() { Reset(); }
+ 
 int32 FBullCowGame::GetMaxTries() const { return MyMaxTries; }
 int32 FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
+int32 FBullCowGame::GetHiddenWordLength() const { return MyHiddenWord.length(); }
+
 
 void FBullCowGame::Reset()
 {
 	constexpr int32 MAX_TRIES = 8;
+	const FString HIDDEN_WORD = "planet";
+
 	MyMaxTries = MAX_TRIES;
-
-	const FString HIDDEN_WORD = "ant";
 	MyHiddenWord = HIDDEN_WORD;
-
 	MyCurrentTry = 1;
 	return;
 }
@@ -27,9 +25,9 @@ bool FBullCowGame::IsGameWon() const
 	return false;
 }
 
-bool FBullCowGame::CheckGuessValidity(FString)
+EWordStatus FBullCowGame::CheckGuessValidity(FString) const
 {
-	return false;
+	return EWordStatus::OK; // TODO make actual error
 }
 
 // receives a VALID guess, incriments turn, and returns count
--- BullCowGame/FBullCowGame.h
+++ BullCowGame/FBullCowGame.h
@@ -11,18 +11,27 @@ struct FBullCowCount
 	int32 Cows = 0;
 };
 
+enum class EWordStatus
+{
+	OK,
+	Not_Isogram,
+	Wrong_Length,
+	Not_Lowercase,
+};
+
 class FBullCowGame {
 public:
 	FBullCowGame(); // constructor
+
 	int32 GetMaxTries() const;
 	int32 GetCurrentTry() const;
+	int32 GetHiddenWordLength() const;
+
 	bool IsGameWon() const;
+	EWordStatus CheckGuessValidity(FString) const; // TODO make a more rich return value.
 
 	void Reset(); // TODO make a more rich return value.
-	bool CheckGuessValidity(FString); // TODO make a more rich return value.
-
 	FBullCowCount SubmitGuess(FString);
-
 
 // Pleasse try and ignore this and focus on the interface abot ^^
 private:
